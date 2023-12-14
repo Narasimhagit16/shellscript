ffc84e187f907efe971418509a59a8a61aafdbb1
Narasimha <narasimha.panthangi16@gmail.com>
2023-12-14 00:33:50 -0500

    first commit

--- arrays.sh
+++ arrays.sh
@@ -7,7 +7,7 @@ echo "1st fruite" ${FRUITS[0]}
 echo "2n fruite" ${FRUITS[1]}
 echo "3rd fruite" ${FRUITS[2]}
 
-for frut in ${FRUITS[]}
+for i in $FRUITS
 do
-    echo "$frut"
+    echo "${FRUITS[i]}"
 done
