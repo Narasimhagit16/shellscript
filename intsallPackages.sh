644d5c599467bc618e199d0bba73c471f4b10f38
Narasimha <narasimha.panthangi16@gmail.com>
2023-12-14 20:48:55 -0500

    first commit

---
+++ intsallPackages.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0-TIMESTAMP.log"
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+ID=$( id -u )
+
+if [ $ID -ne 0]
+then
+    echo "you are not root user"
+    exit 1
+else
+    echo "you are root user"
+fi
+
+Validate()
+{
+    if [ $1 -ne 0]
+    then
+        echo -e "$2 ... $R FAILED $N"
+        exit 
+    else
+        echo -e "$2 ... $R SUCCESS $N"
+    fi
+
+}
+
+yum install git -y &>>LOGFILE
+
+Validate $? "Installing GIT"
+
+
