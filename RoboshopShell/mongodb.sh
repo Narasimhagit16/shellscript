277b37613feaf279fb4bef653f1e1360a74b9742
Narasimha <narasimha.panthangi16@gmail.com>
2023-12-15 22:52:02 -0500

    first commit

--- RoboshopShell/mongodb.sh
+++ RoboshopShell/mongodb.sh
@@ -7,7 +7,7 @@ G="\e[32m"
 Y="\e[33m"
 G="\e[0m"
 
-TIMESTAMP=$date +%F-%H-%M-%S
+TIMESTAMP=$( date +%F-%H-%M-%S ) 
 LOGFILE="/tmp/$0-$TIMESTAMP.log"
 
 if [ $ID -ne 0 ]
@@ -15,7 +15,7 @@ then
     echo "your not ROOT user"
     exit 1
 else
-    echo "your not ROOT user"
+    echo "your  ROOT user"
 fi
 
 Validate()
@@ -35,22 +35,22 @@ cp /home/centos/shellscript/RoboshopShell/mongo.repo /etc/yum.repos.d/mongo.repo
 
 dnf install mongodb-org -y &>>$LOGFILE
 
-Validate $1 "installing MongoDB"
+Validate $? "installing MongoDB"
 
 systemctl enable mongod &>>$LOGFILE
 
-Validate $1 "Enabling MongoDB"
+Validate $? "Enabling MongoDB"
 
 systemctl start mongod &>>$LOGFILE
 
-Validate $1 "starting MongoDB"
+Validate $? "starting MongoDB"
 
 
 sed -i '/127.0.0.1/0.0.0.0/g'  /etc/mongod.conf &>>$LOGFILE
 
-Validate $1 "giving external acces to  MongoDB"
+Validate $? "giving external acces to  MongoDB"
 
 systemctl restart mongod &>>$LOGFILE
 
-Validate $1 "restarting MongoDB"
+Validate $? "restarting MongoDB"
 
