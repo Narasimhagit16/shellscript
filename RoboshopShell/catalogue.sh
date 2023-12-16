a7b889208d55390aae7978899e6a8e69d421595a
Narasimha <narasimha.panthangi16@gmail.com>
2023-12-16 13:46:23 -0500

    first commit

--- RoboshopShell/catalogue.sh
+++ RoboshopShell/catalogue.sh
@@ -29,65 +29,65 @@ Validate()
 }
 
 
-dnf module disable nodejs -y &>>#LOGFILE
+dnf module disable nodejs -y &>>$LOGFILE
 
 Validate $? "Disabile current nodeJS version"
 
-dnf module enable nodejs:18 -y &>>#LOGFILE
+dnf module enable nodejs:18 -y &>>$LOGFILE
 
 Validate $? "enabile nodeJS version18"
 
-dnf install nodejs -y &>>#LOGFILE
+dnf install nodejs -y &>>$LOGFILE
 
 Validate $? "Installing nodeJS"
 
-useradd roboshop &>>#LOGFILE
+useradd roboshop &>>$LOGFILE
 
 Validate $? "Adding USER roboshop"
 
-mkdir /app &>>#LOGFILE
+mkdir /app &>>$LOGFILE
 
 Validate $? "Creating app directory"
 
-curl -o /tmp/catalogue.zip https://roboshop-builds.s3.amazonaws.com/catalogue.zip &>>#LOGFILE
+curl -o /tmp/catalogue.zip https://roboshop-builds.s3.amazonaws.com/catalogue.zip &>>$LOGFILE
 
 Validate $? "Downloading Catalogue Application code"
 
-cd /app &>>#LOGFILE
+cd /app &>>$LOGFILE
 
 Validate $? "Changing app directory"
 
-unzip /tmp/catalogue.zip &>>#LOGFILE
+unzip /tmp/catalogue.zip &>>$LOGFILE
 
 Validate $? "Unzipping Catalogue Application code"
 
-npm install &>>#LOGFILE
+npm install &>>$LOGFILE
 
 Validate $? "Installing dependncies"
 
 #vim /etc/systemd/system/catalogue.service
 
-systemctl daemon-reload &>>#LOGFILE
+systemctl daemon-reload &>>$LOGFILE
 
 Validate $? "Reload daemon"
 
-systemctl enable catalogue &>>#LOGFILE
+systemctl enable catalogue &>>$LOGFILE
 
 Validate $? "Enabling Catalogue service"
 
-systemctl start catalogue &>>#LOGFILE
+systemctl start catalogue &>>$LOGFILE
 
 Validate $? "Starting Catalogue service"
 
-cp /home/crntos/shellscript/  /etc/yum.repos.d/mongo.repo &>>#LOGFILE
+cp /home/crntos/shellscript/  /etc/yum.repos.d/mongo.repo &>>$LOGFILE
 
 
 Validate $? "Copying mongo repo file"
 
-dnf install mongodb-org-shell -y &>>#LOGFILE
+dnf install mongodb-org-shell -y &>>$LOGFILE
 
 Validate $? "Intsalling mongo client"
 
-#mongo --host mongodb.nariops.online </app/schema/catalogue.js &>>#LOGFILE
+#mongo --host mongodb.nariops.online </app/schema/catalogue.js &>>$LOGFILE
 
 #Validate $? "Loading scheme into MongoDB"
