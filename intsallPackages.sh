acdc4d74839a76a6060612b6c76e942c7cef9ba8
Narasimha <narasimha.panthangi16@gmail.com>
2023-12-15 20:50:09 -0500

    first commit

--- intsallPackages.sh
+++ intsallPackages.sh
@@ -10,7 +10,7 @@ N="\e[0m"
 
 ID=$( id -u )
 
-if [ $ID -ne 0]
+if [ $ID -ne 0 ]
 then
     echo "you are not root user"
     exit 1
@@ -20,17 +20,17 @@ fi
 
 Validate()
 {
-    if [ $1 -ne 0]
+    if [ $1 -ne 0 ]
     then
         echo -e "$2 ... $R FAILED $N"
         exit 
     else
-        echo -e "$2 ... $R SUCCESS $N"
+        echo -e "$2 ... $G SUCCESS $N"
     fi
 
 }
 
-yum install git -y &>>LOGFILE
+yum install nginx  -y &>>LOGFILE
 
 Validate $? "Installing GIT"
 
