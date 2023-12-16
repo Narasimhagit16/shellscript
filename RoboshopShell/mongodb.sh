37f190fa3e55b47334be5f5344cd7d3b0ee0bf2a
Narasimha <narasimha.panthangi16@gmail.com>
2023-12-15 22:20:46 -0500

    first commit

---
+++ RoboshopShell/mongodb.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#vim /etc/yum.repos.d/mongo.repo
+
+cp /home/centos/shellscript/roboshop/mogo.repo /etc/yum.repos.d/mongo.repo
+
+#dnf install mongodb-org -y 
+
+#systemctl enable mongod 
+
+#systemctl start mongod
+
+
+
+#sed -i '/127.0.0.1/0.0.0.0/g'  /etc/mongod.conf
+
+#systemctl restart mongod
+
+
+
