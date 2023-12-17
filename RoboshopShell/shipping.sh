b6d7675d7cba53684976030b67605e693cf570de
Narasimha <narasimha.panthangi16@gmail.com>
2023-12-17 17:27:57 -0500

    roboshop commit

--- RoboshopShell/shipping.sh
+++ RoboshopShell/shipping.sh
@@ -40,52 +40,56 @@ else
     echo "roboshop user exist"
 fi
 
-mkdir -p /app
+mkdir -p /app &>>$LOGFILE
 
 Validate $? "Creating app directory"
 
-curl -L -o /tmp/shipping.zip https://roboshop-builds.s3.amazonaws.com/shipping.zip
+curl -L -o /tmp/shipping.zip https://roboshop-builds.s3.amazonaws.com/shipping.zip &>>$LOGFILE
 
 Validate $? "Downloading Shipping Application code"
 
-cd /app
+cd /app &>>$LOGFILE
 
 Validate $? "Changing app directory"
 
-unzip -o /tmp/shipping.zip
+unzip -o /tmp/shipping.zip &>>$LOGFILE
 
 Validate $? "Unzipping Catalogue Application code"
 
-mvn clean package
+mvn clean package &>>$LOGFILE
 
 Validate $? "Creating java package"
 
-mv target/shipping-1.0.jar shipping.jar
+mv target/shipping-1.0.jar shipping.jar &>>$LOGFILE
 
 Validate $? "Renaming JAR file"
 
 #/etc/systemd/system/shipping.service
 
-systemctl daemon-reload
+cp /home/centos/shellscript/RoboshopShell/shipping.service /etc/systemd/system/shipping.service &>>$LOGFILE
+
+Validate $? "Copying shipping service"
+
+systemctl daemon-reload &>>$LOGFILE
 
 Validate $? "Reload daemon"
 
-systemctl enable shipping 
+systemctl enable shipping  &>>$LOGFILE
 
 Validate $? "Enabling Shipping service"
 
-systemctl start shipping 
+systemctl start shipping  &>>$LOGFILE
 
 Validate $? "Starting Shipping service"
 
-dnf install mysql -y
+dnf install mysql -y &>>$LOGFILE
 
 Validate $? "Installing MySQL server"
 
-mysql -h mysql.nariops.online -uroot -pRoboShop@1 < /app/schema/shipping.sql 
+mysql -h mysql.nariops.online -uroot -pRoboShop@1 < /app/schema/shipping.sql &>>$LOGFILE
 
 Validate $? "Loading Shipping scheema into mysql "
 
-systemctl restart shipping
+systemctl restart shipping &>>$LOGFILE
 
 Validate $? "re-starting Shipping service"
