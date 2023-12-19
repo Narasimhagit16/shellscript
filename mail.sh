94fb348e1f1a1f631678bbb91da47d4ce970f9c1
Narasimha <narasimha.panthangi16@gmail.com>
2023-12-19 16:01:46 -0500

    shell

--- mail.sh
+++ mail.sh
@@ -7,6 +7,8 @@ ESCAPE_BODY=$(printf '%s\n' "$BODY" | sed -e 's/[]\/$*.^[]/\\&/g');
 TO_ADDRESS=$4
 SUBJECT=$5
 
+echo "body: $BODY"
+
 FINAL_BODY=$(sed -e "s/TO_TEAM/$TO_TEAM/g" -e "s/ALERT_TYPE/$ALERT_TYPE/g" -e "s/BODY/$ESCAPE_BODY/g" template.html)
 
 echo " fINAL_BODY $FINAL_BODY"
