44243b2657e0aba91ce3e882db2050232a683b30
Narasimha <narasimha.panthangi16@gmail.com>
2023-12-25 11:26:37 -0500

    ec2

--- create-ec2.sh
+++ create-ec2.sh
@@ -4,5 +4,5 @@ HOSTED_ZONE=temp
 DNS=temp
 
 
-aws ec2 run-instances --image-id $AMI --count 1 --instance-type t2.micro  --security-group-ids $SG --tags [{Key=Name,Value=node1}]
+aws ec2 run-instances --image-id $AMI --count 1 --instance-type t2.micro  --security-group-ids $SG "ResourceType=instance,--tags [{Key=Name,Value=node1}]"
 
