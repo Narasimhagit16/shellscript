3e1ea336ce0cc0a8d821d0a275cbc2346b7d8e0d
Narasimha <narasimha.panthangi16@gmail.com>
2023-12-25 11:44:51 -0500

    ec2

--- create-ec2.sh
+++ create-ec2.sh
@@ -4,5 +4,6 @@ HOSTED_ZONE=temp
 DNS=temp
 
 
-aws ec2 run-instances --image-id $AMI --count 1 --instance-type t2.micro  --security-group-ids $SG "ResourceType=instance,--tags [{Key=Name,Value=node1}]"
+aws ec2 run-instances --image-id $AMI --count 1 --instance-type t2.micro  --security-group-ids sg-0173e5458efd4d9c1 --tag-specifications "ResourceType=instance,--tags [{Key=Name,Value=node1}]"
 
+#IP_ADDRESS=$(aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type $INSTANCE_TYPE --security-group-ids sg-087e7afb3a936fce7 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
