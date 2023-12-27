0945222030869c14a25a0accd1a8e6566a248919
Narasimha <narasimha.panthangi16@gmail.com>
2023-12-27 17:37:45 -0500

    git

--- roboshop.sh
+++ roboshop.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-AMI_ID= # Amazon machinne Image ID"
-SG_ID= #Securuty Group ID
-ZONE_ID= #Hoasted Zone ID
-DOMIAN_NAME= # Web server URL
+AMI=ami-03265a0778a880afb
+SG=sg-0173e5458efd4d9c1
+HOSTED_ZONE=Z0601407A5EHLIBKCO7P
+DNS=nariops.online
 
 INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
 
@@ -14,8 +14,10 @@ do
     else
         INSTANCE_TYPE="t2.micro"
     fi
-    IP_ADDRESS=$(aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type $INSTANCE_TYPE --security-group-ids sg-087e7afb3a936fce7 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$I}]" --query 'Instances[0].PrivateIpAddress' --output text)
+    #IP_ADDRESS=$(aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type $INSTANCE_TYPE --security-group-ids sg-087e7afb3a936fce7 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$I}]" --query 'Instances[0].PrivateIpAddress' --output text)
     
+    IP_ADDRESS=$(aws ec2 run-instances --image-id $AMI --count 1 --instance-type t2.micro  --security-group-ids sg-0173e5458efd4d9c1 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
+
     echo "$I: $IP_ADDRESS"
 
     #create R53 record, make sure you delete existing record
