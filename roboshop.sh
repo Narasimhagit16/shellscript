4ffabdbe103735c6d50163d2376547dea22b25cf
Narasimha <narasimha.panthangi16@gmail.com>
2023-12-27 17:41:41 -0500

    git

--- roboshop.sh
+++ roboshop.sh
@@ -16,7 +16,7 @@ do
     fi
     #IP_ADDRESS=$(aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type $INSTANCE_TYPE --security-group-ids sg-087e7afb3a936fce7 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$I}]" --query 'Instances[0].PrivateIpAddress' --output text)
     
-    IP_ADDRESS=$(aws ec2 run-instances --image-id $AMI --count 1 --instance-type t2.micro  --security-group-ids sg-0173e5458efd4d9c1 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
+    IP_ADDRESS=$(aws ec2 run-instances --image-id $AMI --count 1 --instance-type t2.micro  --security-group-ids sg-0173e5458efd4d9c1 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$I}]" --query 'Instances[0].PrivateIpAddress' --output text)
 
     echo "$I: $IP_ADDRESS"
 
